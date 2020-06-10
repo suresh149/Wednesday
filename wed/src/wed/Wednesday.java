50b4229cb33f0f962d423399d6775c0fabea6716
Suresh <Suresh@DESKTOP-NN3DIBH>
2020-06-10 19:52:40 +0530

    hi bro

--- wed/src/wed/Wednesday.java
+++ wed/src/wed/Wednesday.java
@@ -4,6 +4,7 @@ public class Wednesday {
 
 	public static void main(String[] args) {
 		System.out.println("hello github");
+		System.out.println("hi hello");
 		// TODO Auto-generated method stub
 
 	}
